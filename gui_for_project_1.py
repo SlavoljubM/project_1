c3e329909889bd7632d497dea015eac6f080c6ef
SlavoljubM <slavoljubm3@gmail.com>
2020-03-07 21:27:57 +0100

    modification

--- gui_for_project_1.py
+++ gui_for_project_1.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import pandas as pd
+import os
 
 
 height = 500
@@ -14,14 +15,23 @@ canvas.pack()
 frame = tk.Frame(root, bg="#99ccff")
 frame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
 
+
+
 def getCSV():
-	global df
 	df = pd.read_csv("result.csv")
 	print(df)
-		
+	
+def import_files():
+	files = [file for file in os.listdir("./Accounts")]
+	for file in files:
+		print(file)
+file_a = import_files()
+file = tk.StringVar(file_a)
+# csv = getCSV()
+# csv1 = tk.StringVar(csv)
 
 
-label_files = tk.Label(frame, bg="#9933ff")
+label_files = tk.Label(frame, bg="#9933ff",textvariable=file)
 label_files.place(relx=0.05, rely=0.05, relwidth=0.5, relheight=0.9)
 
 label_result = tk.Label(frame, bg="#9966ff")
