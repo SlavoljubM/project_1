ff09b835c1a1d4b484fc4fa0667753b07a732444
SlavoljubM <slavoljubm3@gmail.com>
2020-03-08 21:41:26 +0100

    final

--- gui_for_project_1.py
+++ gui_for_project_1.py
@@ -3,45 +3,51 @@ import pandas as pd
 import os
 
 
-height = 500
-width = 700
+height = 700
+width = 1200
 
-root = tk.Tk()
+if __name__ == "__main__":
 
-canvas = tk.Canvas(root, height=height, width=width)
-canvas.pack()
+	root = tk.Tk()
 
-
-frame = tk.Frame(root, bg="#99ccff")
-frame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
+	canvas = tk.Canvas(root, height=height, width=width)
+	canvas.pack()
 
 
+	frame = tk.Frame(root, bg="#99ccff")
+	frame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.9)
 
-def getCSV():
-	df = pd.read_csv("result.csv")
-	print(df)
-	
-def import_files():
-	files = [file for file in os.listdir("./Accounts")]
-	for file in files:
-		print(file)
-file_a = import_files()
-file = tk.StringVar(file_a)
-# csv = getCSV()
-# csv1 = tk.StringVar(csv)
+	def format_csv(df):
+		return str(df)
+
+	def format_files(file):
+		return str(file)
+		
+		
+	def getCSV():
+		df = pd.read_csv("result.csv")
+		label_result["text"] = format_csv(df)
+		
+	def import_files():
+		files = [file for file in os.listdir("./Accounts")]
+		for file in files:
+			label_files["text"] += format_files(file)
+			
+
+			
+	label_files = tk.Message(frame, bg="#9933ff", justify="left")
+	label_files.place(relx=0.05, rely=0.05, relwidth=0.5, relheight=0.9)
+
+	label_result = tk.Label(frame, bg="#9966ff", justify="left")
+	label_result.place(relx=0.5, rely=0.05, relwidth=0.45, relheight=0.9)
+
+	button = tk.Button(frame, text="Result", command=lambda: getCSV(), bg="gray")
+	button.place(relx=0.5, rely=0, relwidth=0.1, relheight=0.05)
+
+	button1 = tk.Button(frame, text="Invoice", command=lambda: import_files(), bg="gray")
+	button1.place(relx=0.4, rely=0, relwidth=0.1, relheight=0.05)
 
 
-label_files = tk.Label(frame, bg="#9933ff",textvariable=file)
-label_files.place(relx=0.05, rely=0.05, relwidth=0.5, relheight=0.9)
-
-label_result = tk.Label(frame, bg="#9966ff")
-label_result.place(relx=0.5, rely=0.05, relwidth=0.45, relheight=0.9)
-
-button = tk.Button(frame, text="Result", command=getCSV, bg="gray")
-button.place(relx=0.5, rely=0, relwidth=0.1, relheight=0.05)
 
 
-
-
-
-root.mainloop()
+	root.mainloop()
